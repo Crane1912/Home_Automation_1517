7ec55f8bc23c7bcde024c302677dc845d653bee6
caitan <crane1912@gmail.com>
2017-06-08 11:10:14 +0530

    "Home_Lights" Class added

---
+++ HAS/Home_Lights.java
@@ -0,0 +1,12 @@
+package HAS;
+
+public class Home_Lights implements Has_Module{
+
+	@Override
+	public void ONandOFF() {
+		// TODO Auto-generated method stub
+		System.out.println("Home Lights");
+		
+	}
+
+}
